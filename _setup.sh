84e85691aecef3824ae368db164173e98cc3b521
Bertrand Sim <46756885+BertrandSim@users.noreply.github.com>
2022-12-14 18:17:18 +0800

    minor fix for commented dotfile and path

--- _setup.sh
+++ _setup.sh
@@ -3,4 +3,4 @@ ln -s $SCRIPTDIR/dot_inputrc ~/.inputrc
 ln -s $SCRIPTDIR/dot_gitconfig ~/.gitconfig
 ln -s $SCRIPTDIR/dot_bash_aliases ~/.bash_aliases
 ln -s $SCRIPTDIR/dot_ctags ~/.ctags
-# ln -s ./dot_ctags ~/.ctags
+# ln -s $SCRIPTDIR/dot_Rprofile ~/.Rprofile
