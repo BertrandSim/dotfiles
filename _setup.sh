871698eda43674e8fa2a52a1bf055bdb0c4a8041
Bertrand Sim <46756885+BertrandSim@users.noreply.github.com>
2022-12-14 17:52:42 +0800

    add _setup.sh for creation of symlinks to respective files for first-time setup

---
+++ _setup.sh
@@ -0,0 +1,6 @@
+SCRIPTDIR=$(dirname $(realpath "$0"))
+ln -s $SCRIPTDIR/dot_inputrc ~/.inputrc
+ln -s $SCRIPTDIR/dot_gitconfig ~/.gitconfig
+ln -s $SCRIPTDIR/dot_bash_aliases ~/.bash_aliases
+ln -s $SCRIPTDIR/dot_ctags ~/.ctags
+# ln -s ./dot_ctags ~/.ctags
