0398b0a4a7f6aa1cb54055a295ae4b4afd97a06f
Bertrand Sim <46756885+BertrandSim@users.noreply.github.com>
2022-12-22 16:43:51 +0800

    add dot_bashrc

--- _setup.sh
+++ _setup.sh
@@ -1,6 +1,7 @@
 SCRIPTDIR=$(dirname $(realpath "$0"))
 ln -s $SCRIPTDIR/dot_inputrc ~/.inputrc
 ln -s $SCRIPTDIR/dot_gitconfig ~/.gitconfig
+ln -s $SCRIPTDIR/dot_bashrc ~/.bashrc
 ln -s $SCRIPTDIR/dot_bash_aliases ~/.bash_aliases
 ln -s $SCRIPTDIR/dot_ctags ~/.ctags
 # ln -s $SCRIPTDIR/dot_Rprofile ~/.Rprofile
