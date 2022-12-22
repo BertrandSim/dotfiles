c946c982011a109453cb537b18eef72cfe725c00
Bertrand Sim <46756885+BertrandSim@users.noreply.github.com>
2022-12-22 16:44:22 +0800

    add config for evolution mail, and comment on how to rearrange folders

--- _setup.sh
+++ _setup.sh
@@ -5,3 +5,8 @@ ln -s $SCRIPTDIR/dot_bashrc ~/.bashrc
 ln -s $SCRIPTDIR/dot_bash_aliases ~/.bash_aliases
 ln -s $SCRIPTDIR/dot_ctags ~/.ctags
 # ln -s $SCRIPTDIR/dot_Rprofile ~/.Rprofile
+
+
+## rearrange folders in evolution
+## alternatively, in the GUI, Menu > Folder > Edit Sort Order...
+# ln -s $SCRIPTDIR/evolution/mail/folder-tweaks.ini ~/.config/evolution/mail/folder-tweaks.ini
